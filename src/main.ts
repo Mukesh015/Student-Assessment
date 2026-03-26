7871300a63b257226a644fd43a7d65d8ef9f8323
Mukesh015 <rg37090@gmail.com>
2026-03-26 10:30:45 +0530

    complete all

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api-docs', app, document);
-
-  await app.listen(3000);
+  const port = process.env.PORT || 9090;
+  await app.listen(port);
 }
 bootstrap();
